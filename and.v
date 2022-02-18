5287be3858a8e0949e172d114a5d1e0ea2a2f6ea
krishnakumar bhat <79183768+krishnakumarbhat@users.noreply.github.com>
2022-02-18 21:30:56 +0530

    Create and.v

---
+++ and.v
@@ -0,0 +1,33 @@
+module and(a,b,c);
+  input a,b;
+  output c;
+  assign c= a&b ;
+endmodule
+
+module or(a,b,c);
+  input a,b;
+  output c;
+  assign c= a|b ;
+endmodule
+
+module nand(a,b,c);
+  input a,b;
+  output c;
+  assign c= ~(a&b) ;
+endmodule
+
+module nor(a,b,c);
+  input a,b;
+  output c;
+  assign c= ~(a|b) ;
+endmodule
+
+
+module xor(a,b,d,f,c);
+  input a,b;
+  output c;
+  reg d,f;
+  nand a1(a,b,f);
+  or a1(a,b,d);
+  and a1(f,d,c);
+endmodule
